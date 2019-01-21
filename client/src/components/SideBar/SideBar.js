727b13f1504f1a77a3578b91bae086e35dea3502
Dylan Fernandes <dylan.fernandes00@gmail.com>
2019-01-21 16:53:57 -0500

    finished sidebar component

--- client/src/components/SideBar/SideBar.js
+++ client/src/components/SideBar/SideBar.js
@@ -6,11 +6,19 @@ const SideBar = () => {
       <h3 className="sidebar__header">Dylan Fernandes</h3>
       <ul className="sidebar__list">
         <p className="sidebar__list-header">Menu</p>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-        <li>Resume</li>
-        <li>Services</li>
+        <li><a href="https://google.com">Home</a></li>
+        <li><a href="https://google.com">About</a></li>
+        <li><a href="https://google.com">Contact</a></li>
+        <li><a href="https://google.com">Reviews</a></li>
+        <li><a href="https://google.com">Requests</a></li>
+      </ul>
+      <ul className="sidebar__list">
+        <p className="sidebar__list-header">Menu</p>
+        <li><a href="https://google.com">Home</a></li>
+        <li><a href="https://google.com">About</a></li>
+        <li><a href="https://google.com">Contact</a></li>
+        <li><a href="https://google.com">Reviews</a></li>
+        <li><a href="https://google.com">Requests</a></li>
       </ul>
     </div>
   )
