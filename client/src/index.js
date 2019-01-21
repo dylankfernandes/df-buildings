2f5c4038fa9346dfb76d54eb67d7b6b19a8286c8
Dylan Fernandes <dylan.fernandes00@gmail.com>
2019-01-20 22:13:20 -0500

    project setup complete

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Home from './pages/Home/';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './config/main.scss'
+
+ReactDOM.render(<Home />, document.getElementById('root'));
 
---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import SideBar from '../../components/SideBar';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <SideBar />
+      </div>
+    )
+  }
+}
+
+export default Home
