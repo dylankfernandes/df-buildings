5036c673dd1112830c581256f3b8975bad847f3a
Dylan Fernandes <dylan.fernandes00@gmail.com>
2019-01-25 09:15:46 -0500

    finished splash page, need to fix relative to entire page

--- client/src/components/CityCard/CityCard.js
+++ client/src/components/CityCard/CityCard.js
@@ -4,7 +4,7 @@ const CityCard = ({ city, image }) => {
   return (
     <div className="citycard">
       <div className="citycard__image">
-        <img src="https://source.unsplash.com/random?300x300?search=New York" alt="" />
+        <img src={`https://source.unsplash.com/random?300x300?search=${city}`} alt="" />
       </div>
       <div className="citycard__content">
         <p>{city}</p>
--- client/src/components/Update/Update.js
+++ client/src/components/Update/Update.js
@@ -3,7 +3,16 @@ import React from 'react'
 const Update = () => {
   return (
     <div className="update">
-      <p>Hello World</p>
+      <img alt="Want to update?" src={require('../../assets/images/dark-city-image-background.jpg')} />
+      <div className="update__overlay">
+        <div className="update__text">
+          <h3>Interested in Updating?</h3>
+          <p>Find out more about our exclusive premium program</p>
+          <p>
+            <a href="https://google.com">Learn More</a>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
