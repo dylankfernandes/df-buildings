393736df33a00b6a3030168bca5554c8aac8d659
Dylan Fernandes <dylan.fernandes00@gmail.com>
2019-02-01 20:46:06 -0500

    finished backend queries and mutations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -39,11 +39,84 @@ const CityType = new GraphQLObjectType({
   fields: () => ({
     name: { type: GraphQLString },
     location: { type: GraphQLString },
-    cities: {
+    buildings: {
       type: GraphQLList(BuildingType),
       resolve(parent, args) {
         return Building.find({ authorId: parent.id })
       }
     }
   })
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    building: {
+      type: BuildingType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Building.findById(args.id)
+      }
+    },
+    city: {
+      type: CityType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return City.findById(args.id)
+      }
+    },
+    buildings: {
+      type: new GraphQLList(BuildingType),
+      resolve(parent, args) {
+        Building.find({})
+      }
+    },
+    cities: {
+      type: new GraphQLList(CityType),
+      resolve(parent, args) {
+        City.find({})
+      }
+    }
+  }
+})
+
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addBuilding: {
+      type: BuildingType,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        rentable: { type: new GraphQLNonNull(GraphQLBoolean) },
+        rentPrice: { type: new GraphQLNonNull(GraphQLInt) },
+        buildingType: { type: new GraphQLNonNull(GraphQLString) },
+        location: { type: GraphQLString },
+        rating: { type: new GraphQLNonNull(GraphQLInt) },
+      },
+      resolve(parent, args) {
+        let building = new Building({
+          name: args.name,
+          rentable: args.rentable,
+          rentPrice: args.rentPrice,
+          buildingType: args.buildingType,
+          location: args.location,
+          rating: args.rating
+        })
+
+        return building.save()
+      }
+    },
+    addCity: {
+      type: CityType,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        location: { type: GraphQLString }
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+  mutation: Mutation
 })
